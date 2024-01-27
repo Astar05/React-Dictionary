1657c168cfa7a8c632561532b0d3dce0f8a6c96b
Astar05 <nhuynh78@gmail.com>
2024-01-27 08:06:28 -0500

    working on results

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(" ");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    setResults(response.data);
   }
 
   function search(event) {
@@ -26,7 +28,10 @@ export default function Dictionary() {
     <div className="Dictionary">
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} autoFocus={true} />
+        <input type="submit" value="Search" />
       </form>
+
+      <Results results={results} />
     </div>
   );
 }
