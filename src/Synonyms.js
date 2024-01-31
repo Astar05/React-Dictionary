7a0f31f251922060ec46a752d2f8a53fe65e9e05
Astar05 <nhuynh78@gmail.com>
2024-01-30 21:19:00 -0500

    adjusted synonym component css

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -3,11 +3,18 @@ import React from "react";
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <ul className="Synonyms">
-        {props.synonyms.map(function (synonym, index) {
-          return <li key={index}>{synonym}</li>;
+      <div className="Synonyms">
+        <strong>
+          <p>Synonym(s):</p>
+        </strong>
+        {props.synonyms.slice(0, 3).map(function (synonym, index) {
+          return (
+            <ul key={index}>
+              <li>{synonym}</li>
+            </ul>
+          );
         })}
-      </ul>
+      </div>
     );
   } else {
     return null;
